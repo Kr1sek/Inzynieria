3ed7707262d033d75af1b710ba4ead21edef688a
Kr1sek <krystiamoskal@gmail.com>
2023-03-13 14:54:45 +0100

    Dodanie kodu

---
+++ Testy/Models/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Testy.Models
+{
+    public class UserAddress
+    {
+        public string Number{ get; set; }
+        public string Description { get; set; }
+    }
+}
--- Testy/UnitTest1.cs
+++ Testy/UnitTest1.cs
@@ -1,4 +1,6 @@
+using Bogus;
 using System;
+using Testy.Models;
 using Xunit;
 
 namespace Testy
@@ -8,7 +10,15 @@ namespace Testy
         [Fact]
         public void Test1()
         {
-
+            Faker<UserAddress> addressBogus = new Faker<UserAddress>();
+            addressBogus.RuleFor(x => x.Description, y => y.Address.FullAddress());
+            addressBogus.RuleFor(x => x.Number, y => y.Address.CountryCode());
+            var personBogus = new Faker<Models.Person>();
+            personBogus.RuleFor(x => x.Name, y => y.Person.FirstName);
+            personBogus.RuleFor(x => x.Surname, y => y.Person.LastName);
+            personBogus.RuleFor(x => x.Birthday, y => y.Person.DateOfBirth);
+            personBogus.RuleFor(x => x.Address, y => addressBogus.Generate());
+            var hundredPpl = personBogus.Generate(100);
         }
     }
 }
